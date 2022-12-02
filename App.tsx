4b8a67eb02366e50b73f082fb3417c091c5e3948
mav273 <marcusvslourenco@fac.pe.senac.br>
2022-12-02 10:46:04 -0300

    feat: Stack.Navigator added

--- App.tsx
+++ App.tsx
@@ -1,8 +1,25 @@
-import Home  from './src/Home';
+import Mapa  from './src/mapa';
+import Reporte from './src/reporte';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Home/>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Mapa">
+        <Stack.Screen name="Mapa" component={Mapa} />
+        <Stack.Screen name="Reporte" component={Reporte} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+
+// export default function App() {
+//   return (
+//     <Mapa/>
+//   );
+// }
 
