e4eaef20e763fc7b5a370b257fc10923526a6c38
Marcus Vinicius <marcusvslourenco@fac.pe.senac.br>
2022-12-15 13:57:28 -0300

    feat: API connection added; Lib for clusters added

--- src/mapa.tsx
+++ src/mapa.tsx
@@ -1,30 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState,useEffect } from 'react';
 import { Text, View, Button, Platform, StyleSheet,SafeAreaView } from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
+import {Marker} from 'react-native-maps';
+import MapView from "react-native-map-clustering";
 
 
 const Mapa = ({navigation}: {navigation: any}) => {
-  return (
-    <SafeAreaView style={css.container}> 
-    
-      <MapView style={css.map} initialRegion={{
-        latitude: -8.052558407021138,
-        longitude: -34.8851752281189,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}>
-        <Marker
-          coordinate={{
-            latitude: -8.052558407021138,
-            longitude: -34.8851752281189
-          }}
-        />
-      </MapView>
-      <View style={css.buttonContainer}>
-        <Button title="Reporta Ponto" onPress={() => navigation.navigate('Reporte')}/>
-      </View>
-    </SafeAreaView>
-  );
+
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState<any[]>([]);
+      useEffect( () => {
+      fetch('https://servidor-alagamaps.vercel.app/api/pontos/todosSeparados')
+      .then((response) => response.json())
+        .then((json) => setData(json))
+      .catch((error) => console.error(error))
+    },[])
+    return (
+      
+      <SafeAreaView style={css.container}> 
+
+        <MapView style={css.map} initialRegion={{
+          latitude: -8.052558407021138,
+          longitude: -34.8851752281189,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}>
+          {data.map(item => {
+            return (
+            <Marker
+            coordinate={{
+              latitude:item.lat,
+              longitude:item.long
+            }}
+            />
+            )
+          })}
+        </MapView>
+        <View style={css.buttonContainer}>
+          <Button title="Reporta Ponto" onPress={() => navigation.navigate('Reporte')}/>
+        </View>
+      </SafeAreaView>
+    );
+  
 };
 
 const css = StyleSheet.create({
