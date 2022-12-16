182838ab69a14436adfa1d9b06ffb1a442459d45
Marcus Vinicius <marcusvslourenco@fac.pe.senac.br>
2022-12-16 00:28:48 -0300

    feat: Path to map, report and home added

--- App.tsx
+++ App.tsx
@@ -10,17 +10,26 @@ import {
 import SignIn from "./screens/login/loginScreen";
 import ContextWrapper from "./context/ContextWrapper";
 import SignUp from "./screens/signUp/signUp";
-import MainScreen from "./screens/main/mainScreen";
+import Mapa from "./screens/main/mainScreen";
+import Reporte from "./screens/reporte/reporte";
+import Home from "./screens/menu/home";
+
 
 const Stack = createStackNavigator();
 const options: StackNavigationOptions = {
   headerShown: false,
 };
+
+
+
+
+
+
 export default function App() {
   const [currentUser, setCurrentUser]: any = useState();
   const [loading, setLoading]: any = useState(true);
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user:any) => {
       setLoading(false);
       if (user) {
         setCurrentUser(user);
@@ -36,14 +45,23 @@ export default function App() {
   return (
     <ContextWrapper>
       <NavigationContainer>
-        {!currentUser ? (
-          <Stack.Navigator screenOptions={options}>
+      {!currentUser ? (
+          <Stack.Navigator initialRouteName="SignIn" screenOptions={options}>
             <Stack.Screen name="SignIn" component={SignIn} />
             <Stack.Screen name="SignUp" component={SignUp} />
+            <Stack.Screen name="Reporte" component={Reporte} />
+            <Stack.Screen name="Mapa" component={Mapa} />
+            <Stack.Screen name="Home" component={Home} />
+
           </Stack.Navigator>
         ) : (
-          <MainScreen />
-        )}
+          <Stack.Navigator initialRouteName="Mapa" screenOptions={options}>
+            <Stack.Screen name="Mapa" component={Mapa} />
+            <Stack.Screen name="Reporte" component={Reporte} />
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="SignIn" component={SignIn} />
+            <Stack.Screen name="SignUp" component={SignUp} />
+          </Stack.Navigator>        )}
       </NavigationContainer>
     </ContextWrapper>
   );
